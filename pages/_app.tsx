123bb3beb0f1973897a69a80c7f08d5f4001b527
ersinakinci <5427394+ersinakinci@users.noreply.github.com>
2022-10-03 13:14:26 -0700

    Add layout to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import '../styles/globals.css';
 import '../components/builder.ts';
+import { FC, ReactNode } from 'react';
 import type { AppProps } from 'next/app';
+import type { CommonBuilderContentProps } from '../lib/resolve-builder-content';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const Noop: FC<{ children: ReactNode }> = ({ children }) => <>{children}</>;
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ builderContent: CommonBuilderContentProps & { page: any } }>) {
+  const Layout = (Component as any).Layout || Noop;
+
+  return (
+    <Layout pageProps={pageProps}>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
